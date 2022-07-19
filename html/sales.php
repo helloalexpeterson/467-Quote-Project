584a4c2a7859b3a7833ab5271edd5844016700f5
Alexander Peterson <z1912480@students.niu.edu>
2022-07-19 10:30:29 -0500

    rough dropdown feature

--- html/sales.php
+++ html/sales.php
@@ -17,24 +17,19 @@
     $password = "student";
     $dbname = "csci467";
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+    try {
+      $dsn = "mysql:dbname=$dbname;host=$servername";
+      $pdo = new PDO($dsn, $username, $password);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    } 
+    catch(PDOException $error){  
+        die('    <p> Connection to database failed: ' . $error->getMessage() . "</p>\n </body></html>"); 
     }
     
-    $sql = "SELECT * FROM customers";
-    $result = $conn->query($sql);
+    $sql = "SELECT id, name FROM customers";
     
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        echo " " . $row["id"]. "&nbsp" . $row["name"]."<br>";
-      }
-    } else {
-      echo "0 results";
-    }
-    $conn->close();
+    $result = $pdo->query($sql);
+    $row = $result->fetchAll(PDO::FETCH_ASSOC);
     ?>
 
   </head>
@@ -49,26 +44,16 @@
         <option selected="selected">Choose one</option>
         <?php
         // A sample product array
-        $products = array("Mobile", "Laptop", "Tablet", "Camera");
+        //$products = array();
         
         // Iterating through the product array
-        while($row = $result->fetch_assoc()){
-          foreach($row["name"] as $name){
-            echo "<option value=$item>$item</option>";
-          }
-        }
+        foreach($row as $customer => $index){
+          echo "<option value={$index['name']}>{$index['name']}</option>";
+      }
         
         ?>
     </select>
     <input type="submit" value="Submit">
-</form>
 
-
-    <input type="text" id="fname" name="fname" value=""><br>
-    <label for="lname">Password:</label><br>
-    <input type="text" id="lname" name="lname" value=""><br><br>
-    <input type="submit" value="Submit">
-    </form> 
-    
   </body>
 </html>
