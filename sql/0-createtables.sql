ccfd4a93e59762a9760dcc5494b3dcf5ebe28b25
Alexander Peterson <z1912480@students.niu.edu>
2022-07-18 19:00:33 -0500

    Update tables and correct foreign key constraints

--- sql/0-createtables.sql
+++ sql/0-createtables.sql
@@ -1,5 +1,5 @@
 -- Put this at the top
-DROP TABLE IF EXISTS Employees, Quotes, PurchaseOrders, Notes, LineItems;
+DROP TABLE IF EXISTS PurchaseOrders, LineItems, Notes, Quotes, Employees;
 
 --creates tables
 CREATE TABLE Employees (
@@ -13,51 +13,50 @@ CREATE TABLE Employees (
 
 CREATE TABLE Quotes (
 	QuoteID INT PRIMARY KEY AUTO_INCREMENT,
-	CustomerID VARCHAR(32) NOT NULL,
-    EmployeeID VARCHAR(32) NOT NULL,
+	CustomerID INT NOT NULL,
+    EmployeeID INT NOT NULL,
 	Email VARCHAR(64) NOT NULL,
-    Name VARCHAR(32) NOT NULL,
+    CustomerName VARCHAR(32) NOT NULL,
     Addr VARCHAR(32) NOT NULL,
-    Phone VARCHAR(16) NOT NULL,
-    Status ENUM('open', 'finalized', 'sanctioned', 'ordered'),
-    CommissionRate DECIMAL (0,2),
+    Contact VARCHAR(32) NOT NULL,
+    OrderStatus ENUM('open', 'finalized', 'sanctioned', 'ordered'),
+    CommissionRate INT(2),
 
     FOREIGN KEY(EmployeeID) REFERENCES Employees
 );
 
 CREATE TABLE Notes (
-	NOTEID INT PRIMARY KEY AUTO_INCREMENT,
+	NoteID INT PRIMARY KEY AUTO_INCREMENT,
     QuoteID INT, 
 	Note VARCHAR(128),
     
-    FOREIGN KEY(QUOTEID) REFERENCES Quotes
+    FOREIGN KEY(QuoteID) REFERENCES Quotes 
 );
 
 CREATE TABLE LineItems (
-    LineID INT PRIMARY KEY AUTO_INCREMENT;
-
+    LineID INT PRIMARY KEY AUTO_INCREMENT,
     QuoteID INT,
-    Cost DECIMAL(6,2),
+    Cost DECIMAL(7,2),
     Quantity INT,
-    Service VARCHAR(64),
+    ServiceDesc VARCHAR(64),
 
     FOREIGN KEY(QuoteID) REFERENCES Quotes
 );
 
 CREATE TABLE PurchaseOrders(
     PurchaseID INT NOT NULL AUTO_INCREMENT,
-    QuoteID INT NOT NULL,
-    EmployeeID INT NOT NULL,
-    CustomerID INT NOT NULL,
+    QuoteID INT,
+    EmployeeID INT,
+    CustomerID INT,
     OrderTotal DECIMAL(10, 2) NOT NULL,
-    CustomerName VARCHAR(32) NOT NULL,
+    CustomerName VARCHAR(32),
     ProcessDate DATE NOT NULL,
-    CommissionTotal DECIMAL(8, 2) NOT NULL,
-    TimeStamp TIMESTAMP NOT NULL,
+    CommissionTotal DECIMAL(8,2) NOT NULL,
+    OrderTime TIMESTAMP NOT NULL,
 
     PRIMARY KEY (PurchaseID),
     FOREIGN KEY (QuoteID) REFERENCES Quotes (QuoteID),
-    FOREIGN KEY (EmployeeID) REFERENCES Emoloyees (EmployeeID),
-    -- FROM LEGACY CUSTOMER DB
-    -- FOREIGN KEY (CustomerID) REFERENCES !!!!!LEGACY DB!!!!!
+    FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID)
+    
+    -- FOREIGN KEY (CustomerID) must match legacy db 
 );
