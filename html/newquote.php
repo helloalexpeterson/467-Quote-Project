62217c92e5e2ce28b574f1e4d22cc6559b92df79
Alexander Peterson <z1912480@students.niu.edu>
2022-07-19 20:33:43 -0500

    edit new quote page

--- html/newquote.php
+++ html/newquote.php
@@ -21,20 +21,37 @@
    $row = $result->fetchAll(PDO::FETCH_ASSOC);   
 
    //debug print
+   echo "ignore this - debug info";
    echo "<pre>";  print_r($_POST);  echo "<br>";  echo "</pre>";
    echo "<pre>";  print_r($row);    echo "</pre>";
 
+   //Print header
    echo "<h2> New Quote For: {$row[0]['name']} </h2>";
+
+   //Print customer details
    foreach($row[0] as $k => $v){
     if($k !== "id" && $k !== "name"){
         echo $v;
         echo "<br>";
         }
    }
-
-   
-
-   
-   
-
 ?>
+<br>
+<form>  
+    <label for ="email">Email:</label>  
+    <input type="text" id="email" name="email"><br>
+</form>
+
+<p> Quote line items: </p><br>
+<br>
+<p>Secret notes: </p><br>
+<br>
+
+<form>  
+    <label for ="discount">Discount: </label>  
+    <input type="text" id="discount" name="discount"><br> 
+    <input type="radio" id="percent" name="discounttype" value="percent">
+        <label for="percent">Percent off order</label><br>
+    <input type="radio" id="amount" name="discounttype" value="amount">
+        <label for="amount">Amount off order</label><br>
+</form>
