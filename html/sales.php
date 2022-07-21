9eb9080de33e50ae0f9a7a45d6d2bf6fdda669d5
Alexander Peterson <z1912480@students.niu.edu>
2022-07-20 23:41:14 -0500

    add if block in sales.php

--- html/sales.php
+++ html/sales.php
@@ -9,7 +9,7 @@
     <meta name="keywords" content="keywords" />
     <link rel="stylesheet" href="./stylesheet.css" type="text/css" />
     <style type="text/css">.body { width: auto; }</style>
-
+    
   <?php
     include '../lib/db.php';
     $pdo = connectlegacy();
@@ -21,27 +21,49 @@
 
   </head>
   <body>
-    
-    <form action="newquote.php" method="POST">
-    <label for="id">Select customer:</label><br>
-      <select id="id" name="id">
-        <option value="selected">Choose one</option>
-        <?php
-      
+
+
+  <?php 
+  if($view=="associate") 
+  { 
+    echo"<form action='newquote.php' method='POST'>";
+    echo "<label for='id'>Select customer:</label><br>";
+    echo "<select id='id' name='id'>";
+      echo "<option value='selected'>Choose one</option>";
         // Iterating through the array of customers
         foreach($row as $customer => $index){
           echo "<option value={$index['id']}>{$index['name']}</option>";
       }
-        
-        ?>
-    </select>
-    <input type="submit" value="Create New Quote"> <p>This will direct to a new page</p>
+    echo "</select>";  
+     
+    echo "<input type='submit' value='Create New Quote'> <p>This will direct to a new page</p>";
+  }
+  
 
-    <h3>List of open quotes:</h3>
+if($view=="admin") 
+  { 
+   echo "This would be the master quote lookup";
+  }
+  ?>
+
+
+    <?php echo "<h3>List of $querytype quotes:</h3>"?>
     <?php 
     $db = connectdb();
-    $dbsql = "SELECT CustomerID,   , name FROM customers";
-    $dbresult = 
+    $dbsql = "SELECT Quotes.CustomerID, Quotes.CustomerName, Quotes.OrderTotal FROM Quotes WHERE OrderStatus = '$querytype';";
+    $dresult = $db->query($dbsql);
+    $dbrow = $dresult->fetchAll(PDO::FETCH_ASSOC);
+
+   
+     echo "<table border='1'>
+     <tr>
+     <th>ID</th>
+     <th>Name</th>
+     <th>Order Total</th>
+     </tr>";
+     echo "<td> {$dbrow[0]["CustomerID"] } </td>" ; 
+     echo "<td> {$dbrow[0]["CustomerName"] } </td>" ; 
+     echo "<td> {$dbrow[0]["OrderTotal"] } </td>" ; 
 
     
     ?>
