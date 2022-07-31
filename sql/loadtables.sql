7b4a69dba685e06e6ae2379b10e3f344678bed55
Alexander Peterson <z1912480@students.niu.edu>
2022-07-31 14:48:39 -0500

    remove password text field, make file shorter

--- sql/loadtables.sql
+++ sql/loadtables.sql
@@ -1,107 +1,56 @@
-INSERT INTO Employees (Email, EmpName, Title, PwHash, PwText, Street)
-VALUES ('z1912480@niu.edu', 'Alex Peterson', 'Sales Associate', '1234', '1234', '4701 Tator Patch Road');
+INSERT INTO Employees (Email, EmpName, Title, PwHash, Street) VALUES
+    ('z1912480@niu.edu', 'Alex Peterson', 'Sales Associate', '$2y$10$/Jw0MmHmN7XtJ53GdkrjguezqVrgkCdAOoWOPf.ZDR65Syxoc.r1e', '4701 Tator Patch Road'),
+    ('bighead@aol.com', 'John Smith', 'Headquarters', '5678', '4466 North Avenue'),
+    ('me@me.com', 'Jane Doe', 'Administrator', 'abcd', '4154 Sugar Camp Road'),
+    ('megamind@gmail.com', 'Megamind', 'Superuser', 'efgh', '1228 University Drive'),
+    ('z1912480@niu.edu', 'sales', 'Sales Associate', '$2y$10$/Jw0MmHmN7XtJ53GdkrjguezqVrgkCdAOoWOPf.ZDR65Syxoc.r1e',  '3182 Leisure Lane'),
+    ('z1912480@niu.edu', 'hq', 'Headquarters', '$2y$10$/Jw0MmHmN7XtJ53GdkrjguezqVrgkCdAOoWOPf.ZDR65Syxoc.r1e', '3223 Lunetta Street'),
+    ('z1912480@niu.edu', 'admin', 'Administrator', '$2y$10$/Jw0MmHmN7XtJ53GdkrjguezqVrgkCdAOoWOPf.ZDR65Syxoc.r1e', '1516 Green Gate Lane');
 
-INSERT INTO Employees(Email, EmpName, Title, PwHash, Street)
-VALUES ('bighead@aol.com', 'John Smith', 'Headquarters', '5678', '4466 North Avenue');
+INSERT INTO Quotes (CustomerID, CustomerName, City, Street, Contact, Email, EmployeeID, OrderStatus, CommissionRate, OrderTotal, StartDate) VALUES 
+ ('1','IBM Corporation', 'Armonk, NY', 'The IBM Way', '1-800-CALL-IBM', 'ibm@ibm.ibm', '1', 'open', '5', '1000', '2022-05-07'),
+ ('2', 'Ege Consulting, Inc.', 'Miami, FL', '14531 SW 76 Street','www.ege.com', 'ege@egeworld.edu','2', 'finalized', '8', '500', '2019-02-19'),
+ ('2', 'Ege Consulting, Inc.', 'Miami, FL', '14531 SW 76 Street','www.ege.com', 'ege@egeworld.edu','2', 'sanctioned', '4', '2500',  '2020-12-05'),
+ ('2', 'Ege Consulting, Inc.', 'Miami, FL', '14531 SW 76 Street','www.ege.com', 'ege@egeworld.edu','2', 'open', '5', '3700', '2019-11-25'),
+ ('4','Insight Technologies Group','St. Louis, MO','Hollenberg Drive West, Suite 203','info@insight-tech.com', 'info@insight-tech.com','2','ordered', '10', '1000', '2017-11-15'),
+ ('6','Bell South','Atlanta, GA','Braves Parkway','1-305-970-BELL', 'bell@bell.com','1','sanctioned', '7', '2000', '2022-07-15');
 
-INSERT INTO Employees(Email, EmpName, Title, PwHash, Street)
-VALUES ('me@me.com', 'Jane Doe', 'Administrator', 'abcd', '4154 Sugar Camp Road');
+INSERT INTO PurchaseOrders(QuoteID , EmployeeID , CustomerID , OrderTotal , CustomerName , CommissionRate) VALUES
+('5','2','4','1000','Insight Technologies Group', '10' );
 
-INSERT INTO Employees(Email, EmpName, Title, PwHash, Street)
-VALUES ('megamind@gmail.com', 'Megamind', 'Superuser', 'efgh', '1228 University Drive');
+INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc) VALUES
+('1','500','1','New roof' ),
+('1','400','1','New floor' ),
+('1','100','1','Gumball machine' ),
+('2','300','1','New dog' ),
+('2','200','1','New sandwich' ),
+('2','50','1','Lobotomy' ),
+('3','200','1','New cat' ),
+('3','500','1','New ice cream' ),
+('3','780','1','Heart transplant' ),
+('4','1000','1','Distributor Calibration' ),
+('4','100','1','Clean 02 Filter' ),
+('5','800','1','Decontamination Station' ),
+('5','200','4','Fixed Wiring' ),
+('6','125','3','Open Waterways' ),
+('6','200','1','Process Data' );
 
-INSERT INTO Employees (Email, EmpName, Title, PwHash, PwText, Street)
-VALUES ('z1912480@niu.edu', 'sales', 'Sales Associate', '$2y$10$AlFq6Ds2T8sUfknHb2MLu.gAxBg/6yNxpAUqtshqnIf/33.R76nQu ', '1234', '3182 Leisure Lane');
+/*These don't have quotes to match
 
-INSERT INTO Employees (Email, EmpName, Title, PwHash, PwText, Street)
-VALUES ('z1912480@niu.edu', 'hq', 'Headquarters', '$2y$10$M5sPpmx8ZCWULyhJIVrmnOTbIpTQDpEKKChSujzXHOW2X04CjDS6S', '1234', '3223 Lunetta Street');
 
-INSERT INTO Employees (Email, EmpName, Title, PwHash, PwText, Street)
-VALUES ('z1912480@niu.edu', 'admin', 'Administrator', '$2y$10$wk5uu0XMwkT8WuoQsRdPr.afIrp5BM1I2hQ.UjiHyKnLxFT.B7QOy', '1234', '1516 Green Gate Lane');
-
-INSERT INTO Quotes (CustomerID, CustomerName, City, Street, Contact, Email, EmployeeID, OrderStatus, CommissionRate, OrderTotal, StartDate)
-VALUES ('1','IBM Corporation', 'Armonk, NY', 'The IBM Way', '1-800-CALL-IBM', 'ibm@ibm.ibm', '1', 'open', '5', '1000', '2022-05-07');
-
-INSERT INTO Quotes (CustomerID, CustomerName, City, Street, Contact, Email, EmployeeID, OrderStatus, CommissionRate, OrderTotal, StartDate)
-VALUES ('2', 'Ege Consulting, Inc.', 'Miami, FL', '14531 SW 76 Street','www.ege.com', 'ege@egeworld.edu','2', 'finalized', '8', '500', '2019-02-19');
-
-INSERT INTO Quotes (CustomerID, CustomerName, City, Street, Contact, Email, EmployeeID, OrderStatus, CommissionRate, OrderTotal, StartDate)
-VALUES ('2', 'Ege Consulting, Inc.', 'Miami, FL', '14531 SW 76 Street','www.ege.com', 'ege@egeworld.edu','2', 'sanctioned', '4', '2500',  '2020-12-05');
-
-INSERT INTO Quotes (CustomerID, CustomerName, City, Street, Contact, Email, EmployeeID, OrderStatus, CommissionRate, OrderTotal, StartDate)
-VALUES ('2', 'Ege Consulting, Inc.', 'Miami, FL', '14531 SW 76 Street','www.ege.com', 'ege@egeworld.edu','2', 'open', '5', '3700', '2019-11-25');
-
-INSERT INTO Quotes (CustomerID, CustomerName, City, Street, Contact, Email, EmployeeID, OrderStatus, CommissionRate, OrderTotal, StartDate)
-VALUES ('4','Insight Technologies Group','St. Louis, MO','Hollenberg Drive West, Suite 203','info@insight-tech.com', 'info@insight-tech.com','2','ordered', '10', '69.69', '2017-11-15');
-
-INSERT INTO Quotes (CustomerID, CustomerName, City, Street, Contact, Email, EmployeeID, OrderStatus, CommissionRate, OrderTotal, StartDate)
-VALUES ('6','Bell South','Atlanta, GA','Braves Parkway','1-305-970-BELL', 'bell@bell.com','1','sanctioned', '7', '2000', '2022-07-15');
-
-INSERT INTO PurchaseOrders(QuoteID , EmployeeID , CustomerID , OrderTotal , CustomerName , CommissionRate)
-VALUES ('5','2','4','69.69','Insight Technologies Group', '10' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('1','500','1','New roof' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('1','400','1','New floor' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('1','100','1','Gumball machine' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('2','300','1','New dog' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('2','200','1','New sandwich' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('2','50','1','Lobotomy' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('3','200','1','New cat' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('3','500','1','New ice cream' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('3','780','1','Heart transplant' );
--- Here
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('4','1000','1','Distributor Calibration' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('4','100','1','Clean 02 Filter' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('5','800','1','Decontamination Station' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('5','200','4','Fixed Wiring' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('6','125','3','Open Waterways' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
-VALUES ('6','200','1','Process Data' );
-
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
 VALUES ('7','540','2','Unlock Manifolds' );
 
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
+
 VALUES ('7','8100','1','Reactor Maintenance' );
 
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
+
 VALUES ('8','260','1','Repair Drill' );
 
-INSERT INTO LineItems(QuoteID, Cost, Quantity, ServiceDesc)
+
 VALUES ('8','780','1','Stabilize Steering' );
 
-INSERT INTO Notes (QuoteID, Note)
-VALUES ('1', "Customer is very rude");
-
-INSERT INTO Notes (QuoteID, Note)
-VALUES ('1', "IBM is the literal worst");
-
-INSERT INTO Notes (QuoteID, Note)
-VALUES ('1', "Do not call back");
+*/
+INSERT INTO Notes (QuoteID, Note) VALUES 
+('1', "Customer is very rude"),
+('1', "IBM is the literal worst"),
+('1', "Do not call back");
